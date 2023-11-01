791abf1ff4a9c8860eb7adf73ab4137abec5347f
AnushaHk24 <hkanusha.1999@gmail.com>
2023-11-01 11:33:32 +0530

    changes

--- src/main/java/com/cg/Application.java
+++ src/main/java/com/cg/Application.java
@@ -2,12 +2,21 @@ package com.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
-
-	public static void main(String[] args) {
+	
+	@GetMapping("/message")
+	public String displayMessage() {
+		return "Congratulations you successfully deployed";
+	}
+	
+	public static void main(String [] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 
 }
